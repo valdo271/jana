9a013a4ae332a6534b141088e03a54fbedf2cc9b
valdo27 <v.hlavaty@gmail.com>
2013-06-23 20:57:59 +0200

    vloz

---
+++ Models/test/Note.cs
@@ -0,0 +1,10 @@
+namespace Website.Models
+{
+    public class Note
+    { //druha etapa
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+   //jana 2
+}
