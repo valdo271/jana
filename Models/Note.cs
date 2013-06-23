2908d5e38f49df4c9f5bbd101f4aecd13958f81b
valdo27 <v.hlavaty@gmail.com>
2013-06-23 20:18:27 +0200

    qq
    qq

---
+++ Models/Note.cs
@@ -0,0 +1,10 @@
+namespace Website.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+   //ppp
+}
